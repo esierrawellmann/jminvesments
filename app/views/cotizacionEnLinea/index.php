d4d534c5f4083f84157176c4261dbe6fba6a264b
Brian <Brian@E1572P>
2014-11-10 20:07:29 -0600

    Matenimiento propiedades

--- app/views/cotizacionEnLinea/index.php
+++ app/views/cotizacionEnLinea/index.php
@@ -8,6 +8,31 @@
 <script>
         var app = angular.module('rol', ['ngRoute']);
 	angular.module('rol', ['ui.bootstrap']);
+         function controller($scope, $modal, $log , $http)
+	 {
+             
+             $scope.calculo = function(){
+                $scope.enganche =$scope.precio *0.3;
+                $scope.financiar = $scope.precio - $scope.enganche;
+            }
+            
+            $scope.reserva = function(){
+                $scope.saldo =$scope.enganche - $scope.reservas;
+            } 
+            
+            $scope.pmt = function(fv, type) {
+                
+                    var i = $scope.tasa/12; 
+                    var n = $scope.plazo;
+                    var p = $scope.financiar * (-1);
+                    
+                    var pmt = ((p * i)/(100 * (1-(1 + Math.pow((i/100),-n)))));
+
+                    $scope.cuota = pmt;
+                    $scope.calificar = $scope.cuota * 3.3;
+                   }
+             
+         }
         
 </script>
 
@@ -26,41 +51,131 @@
                    <div class="panel-heading"><h6 class="panel-title">Apartamento Torre Elgin</h6></div>
                  <div class="panel-body">
                      
-                      <form role="form">
-                        <div class="form-group">
-                          <label for="inputEmail3" class="col-sm-6 control-label">Modelo</label>
-                          <div class="col-sm-3">
-                            <input type="text" class="form-control" id="inputEmail3" placeholder="Modelo">
-                          </div>
-                        </div>
-                        <div class="form-group">
-                          <label for="inputPassword3" class="col-sm-6 control-label">Nivel</label>
-                          <div class="col-sm-3">
-                            <input type="text" class="form-control" id="inputPassword3" placeholder="Nivel">
-                          </div>
-                        </div>
-                          <div class="form-group">
-                          <label for="inputEmail4" class="col-sm-6 control-label">Metros Cuadrados de Construccion</label>
-                          <div class="col-sm-3">
-                            <input type="text" class="form-control" id="inputEmail3" placeholder="Mts.2">
-                          </div>
-                        </div>
-                          
-                           <div class="form-group">
-                          <label for="inputEmail5" class="col-sm-6 control-label">No. de Dormitorios</label>
-                          <div class="col-sm-3">
-                            <input type="text" class="form-control" id="inputEmail5" placeholder="Dormitorios">
-                          </div>
-                        </div>
-                          
-                          <div class="form-group">
-                          <label for="inputEmail6" class="col-sm-6 control-label">No. de Parqueos</label>
-                          <div class="col-sm-3">
-                            <input type="text" class="form-control" id="inputEmail6" placeholder="Parqueos">
-                          </div>
-                        </div>
-                          
-                      </form>
+                     <div class="modal-header">
+                        <h3 class="modal-title">Proyecciones para su Inversion</h3>
+                    </div>
+                    <div class="modal-body">
+                        <form role="form" name="userForm">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="userStatus">Modelo</label>
+                                        <input type="text" class="form-control" ng-model="rol.area" id="exampleInputEmail1" placeholder="Modelo"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="negocio">Nivel</label>
+                                        <input type="text" class="form-control" ng-model="rol.area" id="exampleInputEmail1" placeholder="Nivel"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="zona">Mts² de Construccion</label>
+                                        <input type="text" class="form-control" ng-model="rol.area" id="exampleInputEmail1" placeholder="Mts² de Construccion"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="nombre_proyecto">No. Dormitorios</label>
+                                        <input type="text" class="form-control" name="nombre_proyecto" required="true" ng-model="new.nombre_proyecto" id="nombre_proyecto" placeholder="No. de Dormitorios"/>          
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="nombre_propietario">No. de Parqueos</label>
+                                        <input type="text" class="form-control" required="true" ng-model="new.nombre_propietario" id="nombre_propietario" placeholder="No. de Parqueos"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="dormitorios">No. de Dormitorios</label>
+                                        <input type="text" class="form-control" type="number" ng-model="new.dormitorios" id="dormitorios" name="dormitorios" placeholder="Numero de Dormitorios"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Bodega</label>
+                                        <input type="text" class="form-control" type="number" ng-model="rol.precio_renta" id="exampleInputEmail1" placeholder="Bodega"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Precio + IVA</label>
+                                        <input type="text" class="form-control" type="number" ng-model="precio" ng-change="calculo()" id="exampleInputEmail1" placeholder="Precio"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Enganche</label>
+                                        <input type="text" class="form-control" ng-model="enganche" id="exampleInputEmail1" placeholder="Enganche"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Reserva</label>
+                                        <input type="text" class="form-control" ng-model="reservas" ng-change="reserva()" id="exampleInputEmail1" placeholder="Reserva"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Saldo Enganche</label>
+                                        <input type="text" class="form-control" ng-model="saldo" id="exampleInputEmail1" placeholder="Saldo Enganche"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Saldo Para Financiar</label>
+                                        <input type="text" class="form-control" ng-model="financiar" id="exampleInputEmail1" placeholder="Saldo a Financiar"/>
+                                    </div>
+                                </div>
+                            </div>  
+                            
+                             <div class="modal-header">
+                                <h3 class="modal-title">Financiamiento Proyectado</h3>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Plazo en Meses</label>
+                                        <input type="text" class="form-control" ng-model="plazo" ng-change="pmt()" id="exampleInputEmail1" placeholder="Plazo en Meses"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Tasa Variable</label>
+                                        <input type="text" class="form-control" ng-model="tasa" ng-change="pmt()" id="exampleInputEmail1" placeholder="Tasa Variable"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Cuota Mensual Financiamiento</label>
+                                        <input type="text" class="form-control" ng-model="cuota" id="exampleInputEmail1" placeholder="Financiamiento"/>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Ingreso Estimado para Calificar en el banco</label>
+                                        <input type="text" class="form-control" ng-model="calificar" id="exampleInputEmail1" placeholder="Saldo a Financiar"/>
+                                    </div>
+                                </div>
+                            </div> 
+                            
+                        </form>
+                    </div>   
                  </div>
                  
                     </div>
