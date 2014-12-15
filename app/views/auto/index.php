0ffac9b4a1aa80dbf1858c14d87646cfa0faff58
Brian <Brian@E1572P>
2014-12-14 18:16:36 -0600

    Merge origin/master
    Conflicts:
    	backend/app/views/cotizador/index.php

---
+++ app/views/auto/index.php
@@ -0,0 +1,26 @@
+<?php  include("../header.php"); ?> 
+<div class="row" style="padding-top: 20px;">
+
+
+                <div class="col-lg-6" style="">
+
+                    <div class="well" style="height:200px; overflow-y:auto; cursor:pointer;" onclick="singleView('Apartamento')">
+                    <h2 style="margin-top: 0;">Venta</h2>
+                    <img class="img-responsive" src="apartamento.JPG">
+                        </div>
+
+                    </div>
+                
+                <div class="col-lg-6" style="">
+
+                    <div class="well" style="height:200px; overflow-y:auto; cursor:pointer;" onclick="singleView('Bodega')">
+                     <h2 style="margin-top: 0;">Renta</h2>
+                     <img class="img-responsive" src="bodega.jpg">
+                        </div>
+
+                    </div>
+                
+                
+
+                </div>
+<?php  include("../footer.php"); ?>
